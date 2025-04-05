582c10b6539f69ea8daf16b8f21987c194f2112e
Greem4 <i@greem4.ru>
2025-04-05 16:16:08 +0300

    project init

---
+++ src/test/java/ru/greemlab/neirocalendar/NeiroCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.greemlab.neirocalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeiroCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
